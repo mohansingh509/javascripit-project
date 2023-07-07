abe076fd8db8dddbed9e218ff250ce8f968fb8e9
Mohan Singh <ms8941798@gmail.com>
2023-07-07 15:04:04 +0530

    added file

---
+++ weather app/app.js
@@ -0,0 +1,30 @@
+const API_key="ea714956e2e8f454c7c5edc056ac2436"
+const form = document.querySelector("form")
+const search = document.querySelector("#search")
+const weather = document.querySelector("#weather")
+
+const getWeather = async (city) =>{
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}&units=metric`
+  response = await fetch(url);
+  const data =await response.json()
+  return showWeather(data) 
+}
+const showWeather = (data) =>{
+  console.log(data)
+  weather.innerHTML = `
+  <div class="row" id="weather">
+      <div><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt=""></div>
+      <div>
+        <h2>${data.main.temp}°C</h2>
+        <h4>${data.weather[0].main}</h4>
+      </div>
+    </div>
+  `
+}
+form.addEventListener(
+    "submit",
+    function(event){
+        getWeather(search.value)
+        event.preventDefault();
+    }
+)
